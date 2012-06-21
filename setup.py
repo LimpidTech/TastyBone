63acf55d7f7064a3f1d55b13abadefb680e65912
Brandon R. Stoner <bstoner@yrsi.com>
2012-06-21 15:51:49 -0600

    Proper version bump.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ for filename in metafiles:
 
 metadata = {
     'name': 'tastybone',
-    'version': '0.0.2',
+    'version': '0.0.3',
     'description': 'Expose TastyPie to your client. Nicely.',
     'long_description': metafiles['README.md'] + '\n\n' + metafiles['CHANGES.md'],
     'classifiers': metafiles['CLASSIFIERS.txt'],
