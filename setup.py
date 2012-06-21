e4e0f76e0aa61c7dda7d8a75e47944e6ec002cc3
Brandon R. Stoner <bstoner@yrsi.com>
2012-06-21 05:14:32 -0600

    Modified setup process quite a bit.

--- setup.py
+++ setup.py
@@ -6,33 +6,33 @@ import sys
 parent_directory = os.path.abspath(os.path.dirname(__file__))
 
 metafiles = {
-	'README.md': None,
-	'CHANGES.md': None,
-	'CLASSIFIERS.txt': None,
+    'README.md': None,
+    'CHANGES.md': None,
+    'CLASSIFIERS.txt': None,
 }
 
 # The following bit will read each index from metafiles and fill it's value
 # with the contents of that file if it is able to read the file.
 for filename in metafiles:
-	try:
-		current_file = open(os.path.join(parent_directory, filename))
-		metafiles[filename] = current_file.read()
-		current_file.close()
+    try:
+    	current_file = open(os.path.join(parent_directory, filename))
+    	metafiles[filename] = current_file.read()
+    	current_file.close()
 
-	except IOError:
-		pass
+    except IOError:
+    	pass
 
 metadata = {
-	'name': 'tastybone',
-	'version': '0.0.1',
-	'description': 'Expose TastyPie to your client. Nicely.',
-	'long_description': metafiles['README.md'] + '\n\n' + metafiles['CHANGES.md'],
-	'classifiers': metafiles['CLASSIFIERS.txt'],
-	'author': 'Brandon R. Stoner',
-	'author_email': 'monokrome@limpidtech.com',
-	'url': 'http://github.com/LimpidTech/TastyBone',
-	'keywords': 'django tastypie',
-	'packages': ['tastybone'],
+    'name': 'tastybone',
+    'version': '0.0.1',
+    'description': 'Expose TastyPie to your client. Nicely.',
+    'long_description': metafiles['README.md'] + '\n\n' + metafiles['CHANGES.md'],
+    'classifiers': metafiles['CLASSIFIERS.txt'],
+    'author': 'Brandon R. Stoner',
+    'author_email': 'monokrome@limpidtech.com',
+    'url': 'http://github.com/LimpidTech/TastyBone',
+    'keywords': 'django tastypie',
+    'packages': ['tastybone'],
     'package_data': {
         'tastybone': [
             'static/scripts/tastybone.js',
